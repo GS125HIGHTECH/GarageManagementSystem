e8aabdae49061e73890c76a2418dda7fb73df574
GS125HIGHTECH <grzegorz.sobczak214@gmail.com>
2025-12-30 22:00:01 +0100

    Update Main class to initialize database

--- src/main/java/pl/sobczak/grzegorz/Main.java
+++ src/main/java/pl/sobczak/grzegorz/Main.java
@@ -1,6 +1,21 @@
 package pl.sobczak.grzegorz;
 
+import pl.sobczak.grzegorz.dao.UserDao;
+import pl.sobczak.grzegorz.db.DatabaseConnection;
+import pl.sobczak.grzegorz.model.User;
+
 public class Main {
     public static void main(String[] args) {
+        DatabaseConnection.initDatabase();
+
+        try {
+            User testUser = new User("Jan", "Kowalski", "jan@kowalski.pl", "test123");
+
+            UserDao userDao = new UserDao();
+            userDao.saveUser(testUser);
+
+        } catch (IllegalArgumentException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 }
