25537ef5e4e1a81d4241ff0d504f26dc879cbf78
GS125HIGHTECH <grzegorz.sobczak214@gmail.com>
2026-01-04 11:12:57 +0100

    Update Main to demonstrate system workflow

--- src/main/java/pl/sobczak/grzegorz/Main.java
+++ src/main/java/pl/sobczak/grzegorz/Main.java
@@ -1,8 +1,15 @@
 package pl.sobczak.grzegorz;
 
+import pl.sobczak.grzegorz.dao.RepairOrderDao;
 import pl.sobczak.grzegorz.dao.UserDao;
+import pl.sobczak.grzegorz.dao.VehicleDao;
 import pl.sobczak.grzegorz.db.DatabaseConnection;
 import pl.sobczak.grzegorz.model.User;
+import pl.sobczak.grzegorz.model.Vehicle;
+import pl.sobczak.grzegorz.model.RepairOrder;
+import pl.sobczak.grzegorz.service.RepairOrderService;
+import pl.sobczak.grzegorz.service.UserService;
+import pl.sobczak.grzegorz.service.VehicleService;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -12,10 +19,46 @@ public class Main {
         DatabaseConnection.initDatabase();
 
         try(Connection conn = DatabaseConnection.getConnection()) {
-            User testUser = new User("Jan", "Kowalski", "jan@kowalski.pl", "test123");
-
             UserDao userDao = new UserDao(conn);
-            userDao.saveUser(testUser);
+            VehicleDao vehicleDao = new VehicleDao(conn);
+            RepairOrderDao repairDao = new RepairOrderDao(conn);
+
+            UserService userService = new UserService(userDao);
+            VehicleService vehicleService = new VehicleService(vehicleDao, userDao);
+            RepairOrderService repairService = new RepairOrderService(repairDao, vehicleDao);
+
+            User testUser = userService.registerNewUser("Jan", "Kowalski", "jan@kowalski.pl", "test123");
+            User testInactiveUser = userService.registerNewUser("Jan", "Nowak", "jan@nowak.pl", "test123");
+            User testAdminUser = userService.registerNewUser("Adam", "Nowak", "adam@nowak.pl", "test123");
+
+            userService.changeUserRole(testAdminUser.getEmail(), "ADMIN");
+            userService.deactivateUser(testInactiveUser.getEmail());
+
+            userDao.getUserByEmail("jan@nowak.pl").ifPresent(user ->
+                    System.out.println(user.isActive())
+            );
+
+            userDao.getUserByEmail("adam@nowak.pl").ifPresent(user ->
+                    System.out.println(user.getRole())
+            );
+
+            Vehicle a1 = vehicleService.registerNewVehicle(testUser.getUserId(), "Dodge", "Ram", "2B6HB21Y8LK730520", "White");
+            Vehicle a2 = vehicleService.registerNewVehicle(testUser.getUserId(), "Honda", "Civic", "JH2SC68W6EK000230", "Red");
+
+            Vehicle b1 = vehicleService.registerNewVehicle(testAdminUser.getUserId(), "Toyota", "Camry", "4T1BF1FK8DU251252", "Blue");
+
+            RepairOrder ro1 = new RepairOrder(a1.getId(), "repair1", 250.0);
+            RepairOrder ro2 = new RepairOrder(a2.getId(), "repair2", 1000.0);
+            RepairOrder ro3 = new RepairOrder(b1.getId(), "repair3", 100.0);
+
+            repairService.createOrder(ro1);
+            repairService.createOrder(ro2);
+            repairService.createOrder(ro3);
+
+            repairService.completeRepair(ro1.getId());
+
+            System.out.println(repairService.getTotalRepairCostsForVehicle(a1.getId()) + " PLN");
+            System.out.println(vehicleService.getVehiclesByOwner(testUser.getUserId()).size());
 
         } catch (IllegalArgumentException | SQLException e) {
             System.err.println("Error: " + e.getMessage());
